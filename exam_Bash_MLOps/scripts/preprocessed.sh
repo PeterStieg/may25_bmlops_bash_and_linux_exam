5263eb93adca5cab0ccd55e404c8011ccbc909bb
Peter Stieg <PeterStieg@gmx.de>
2025-06-24 19:55:48 +0000

    Peter: Added preprocessed.sh

--- exam_Bash_MLOps/scripts/preprocessed.sh
+++ exam_Bash_MLOps/scripts/preprocessed.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 # =============================================================================
 # This script preprocessed.sh runs the program src/preprocessed.py
 # and logs the execution details in the log file
 # logs/preprocessed.logs.
 # =============================================================================
+python3 ../src/preprocessed.py
