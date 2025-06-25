59d07e6f8c6a4f00ebab1a3bb23a23836b7dedbe
Peter Stieg <PeterStieg@gmx.de>
2025-06-26 01:01:56 +0200

    Peter: Added virtual environment to Shell scripts

--- exam_Bash_MLOps/scripts/preprocessed.sh
+++ exam_Bash_MLOps/scripts/preprocessed.sh
@@ -4,4 +4,9 @@
 # and logs the execution details in the log file
 # logs/preprocessed.logs.
 # =============================================================================
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the preprocessing script
 python3 ../src/preprocessed.py
--- exam_Bash_MLOps/scripts/train.sh
+++ exam_Bash_MLOps/scripts/train.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 # -----------------------------------------------------------------------------
 # This script train.sh runs the Python program src/train.py.
 # This program trains a prediction model and saves the final model
 # in the model/ directory. The script also logs all execution details
 # in the file logs/train.logs.
 # -----------------------------------------------------------------------------
-python3 ../src/train.py
+
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the training script
+python ../src/train.py
